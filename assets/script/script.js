074460823639c9e4ad461a994cac48367224853c
ArtSb2604 <artsb2604@gmail.com>
2023-12-18 13:29:45 +0300

    commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,48 @@
+
+
+const mySwiper = new Swiper('.swiper-category', {
+  spaceBetween: 30,
+  initialSlide: 0,
+  centerInsufficientSlides: true,
+  loop: false,
+  freeMode: true,
+  freeModeSticky: true,
+  grid: {
+    fill: 'row',
+    rows: '2'
+  },
+  slidesPerView: 2,
+});
+
+
+
+var productThumbs = new Swiper(".product-thumbs", {
+  spaceBetween: 30,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
+var productSlider = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 1,
+  loopedSlides: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: productThumbs,
+  },
+  
+});
+
+
+
 const spaceHolder = document.querySelector('.space-holder');
 const horizontal = document.querySelector('.horizontal');
 spaceHolder.style.height = `${calcDynamicHeight(horizontal)}px`;
